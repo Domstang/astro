c5ecdda148e316a99c4c17bc4effebef3220a3d0
Domstang <arnaudlecamus@gmail.com>
2023-09-12 11:13:43 +0200

    Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
