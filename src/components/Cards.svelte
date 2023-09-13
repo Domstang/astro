b217b4c3d2776970f8f6b2a23bdb7d2bbe1c1370
Domstang <arnaudlecamus@gmail.com>
2023-09-13 09:13:12 +0200

    add full image

---
+++ src/components/Cards.svelte
@@ -0,0 +1,13 @@
+<script>
+    let cardsNumber = 20;
+
+</script>
+<div id="cards" class="px-10 flex justify-center items-center mb-10">
+    <div class="flex flex-wrap justify-between gap-5">
+    {#each Array(cardsNumber) as _, i}
+        <div class="h-96 w-72 bg-gray-300">
+            Card
+        </div>
+    {/each}
+    </div>
+</div>
