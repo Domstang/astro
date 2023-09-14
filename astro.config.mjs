096cdfaab36f8b2cdebe192da957a2d28dd44e4b
Domstang <arnaudlecamus@gmail.com>
2023-09-14 09:43:51 +0200

    function per route vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    functionPerRoute: false,
+  }),
 });
